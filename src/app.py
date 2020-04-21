b57ffdac2273f5c901533bcbc801ee69019a8d58
kenderperez <kenderperez1995@gmail.com>
2020-04-21 02:59:45 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from bson import json_util
 from flask import jsonify
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://localhost/users'
+app.config['MONGO_URI']='mongodb://localhost/pythonapiest/users'
 mongo = PyMongo(app)
 
 #RUTAS
